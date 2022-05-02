0a4b535b7d15b2e24b5ddce43cb276a2b2e38259
Turmunkh <turuuu@Turuu.local>
2022-05-02 23:25:43 +0800

    ColorChange

---
+++ color.js
@@ -0,0 +1,36 @@
+console.log("working");
+const box = document.getElementById("box");
+
+const btn = document.getElementById("budag");
+
+
+box.addEventListener('click' , function () {
+    btn.style.backgroundColor = "black";
+    btn2.style.backgroundColor = "red";
+    btn3.style.backgroundColor = "pink";
+    btn4.style.backgroundColor = "orenge";
+    btn5.style.backgroundColor = "salmon";
+    btn6.style.backgroundColor = "#f1c0e8";
+    btn7.style.backgroundColor = "#264653";
+    btn8.style.backgroundColor = "#7209b7";
+    btn9.style.backgroundColor = "#03045e";
+    btn10.style.backgroundColor = "#495057";
+    btn11.style.backgroundColor = "#bb3e03";
+    btn12.style.backgroundColor = "#b9fbc0";
+});
+
+const btn2 = document.getElementById("budag2");
+const btn3 = document.getElementById("budag3");
+const btn4 = document.getElementById("budag4");
+const btn5 = document.getElementById("budag5");
+const btn6 = document.getElementById("budag6");
+const btn7 = document.getElementById("budag7");
+const btn8 = document.getElementById("budag8");
+const btn9 = document.getElementById("budag9");
+const btn10 = document.getElementById("budag!");
+const btn11 = document.getElementById("budag@");
+const btn12 = document.getElementById("budag#");
+
+// colors.addEventListener('click' , function () {
+//     btn2.style.backgroundColor = "red";
+// });
